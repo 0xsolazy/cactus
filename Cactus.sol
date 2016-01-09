d217b4263e658879746bac3dfdbf164e8fa3e76f
0xsolazy <0xsolazy@gmail.com>
2016-01-09 22:11:55 +0100

    player queue

--- Cactus.sol
+++ Cactus.sol
@@ -1,4 +1,12 @@
 contract Cactus {
+    struct Player {
+        address addr;
+        uint256 payout;
+    }
+
+    Player[] players;
+    uint256 public payoutIndex;
+
     address creator;
 
     function Cactus() {
