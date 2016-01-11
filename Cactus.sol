2f4dc888e9629a72eed51f9fd12b29efd2425498
0xsolazy <0xsolazy@gmail.com>
2016-01-11 16:34:08 +0100

    fallback distributes the pot

--- Cactus.sol
+++ Cactus.sol
@@ -12,4 +12,16 @@ contract Cactus {
     function Cactus() {
         creator = msg.sender;
     }
+
+    function () {
+        if (msg.value < 0.5 ether) throw;
+
+        uint256 payout = msg.value * 120 / 100;
+        players.push(Player(msg.sender, payout));
+
+        while (payoutIndex < players.length && this.balance >= players[payoutIndex].payout) {
+            if (!players[payoutIndex].addr.send(players[payoutIndex].payout)) throw;
+            payoutIndex++;
+        }
+    }
 }
