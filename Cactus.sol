8258018c6b41dc46bdaace8fc6fab0f437d46519
0xsolazy <0xsolazy@gmail.com>
2016-01-13 12:08:42 +0100

    owner fee on each entry

--- Cactus.sol
+++ Cactus.sol
@@ -6,6 +6,9 @@ contract Cactus {
 
     Player[] players;
     uint256 public payoutIndex;
+    uint256 public collectedFees;
+
+    uint256 ownerFee = 5;
 
     address creator;
 
@@ -16,10 +19,12 @@ contract Cactus {
     function () {
         if (msg.value < 0.5 ether) throw;
 
+        collectedFees += msg.value * ownerFee / 100;
+
         uint256 payout = msg.value * 120 / 100;
         players.push(Player(msg.sender, payout));
 
-        while (payoutIndex < players.length && this.balance >= players[payoutIndex].payout) {
+        while (payoutIndex < players.length && this.balance >= collectedFees + players[payoutIndex].payout) {
             if (!players[payoutIndex].addr.send(players[payoutIndex].payout)) throw;
             payoutIndex++;
         }
